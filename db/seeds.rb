84a51e1a762afbdf09f9962457561753e51f2df1
Sergey Babich <babichsa.ua@gmail.com>
2018-10-14 16:04:43 +0300

    fix tempus-dominus in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -127,9 +127,9 @@ end
 
 # seed_users
 # seed_groups
-seed_students
-seed_instructors
-seed_drivings
+# seed_students
+# seed_instructors
+# seed_drivings
 
 # Student.delete_all
 # Instructor.delete_all
